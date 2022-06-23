ae302b26ab3951950bd6cff6d58f82399ebea093
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-23 15:53:12 +0200

    Create relationship functions:
    - category()
    - posts()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -12,5 +13,10 @@ class Post extends Model
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
